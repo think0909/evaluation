8a5602a0194b26bfc5913a1b3e2309a2d9c2f04d
cgs.sjtu <cgs.sjtu@gmail.com>
2012-12-12 22:51:32 +0800

    学生信息批量上传功能

--- App/Lib/Action/StudentAction.class.php
+++ App/Lib/Action/StudentAction.class.php
@@ -72,4 +72,58 @@ class StudentAction extends Action
             $this->error('用户名不存在', U('Student/manage'));
         }
     }
+
+    public function upload()
+    {
+        import('ORG.Net.UploadFile');
+        $upload = new UploadFile(); // 实例化上传类
+        $upload->maxSize = 3145728; // 设置附件上传大小
+        $upload->allowExts = array('xls', 'xlsx'); // 设置附件上传类型
+        $upload->savePath = './Upload/'; // 设置附件上传目录
+        if (!$upload->upload()) { // 上传错误提示错误信息
+            $this->error($upload->getErrorMsg(), U('Student/manage'));
+        } else { // 上传成功 获取上传文件信息
+            $info = $upload->getUploadFileInfo();
+            $file = $info[0];
+            $path = $file['savepath'] . $file['savename'];
+            //Read Excel
+            try {
+                Vendor("PHPExcel.PHPExcel.IOFactory");
+                $objPHPExcel = PHPExcel_IOFactory::load($path);
+                $sheetData = $objPHPExcel->getActiveSheet()->toArray(null, true, true, true);
+                //规定：第一行是表头，第一列是学号，第二列是成绩
+                $result = array();
+                $errCount = 0;
+                $model = D('Student');
+                for ($i = 2; $i <= count($sheetData); $i++) {
+                    $line = $sheetData[$i];
+                    $data = array();
+                    $data['id'] = $line['A'];
+                    $data['name'] = $line['B'];
+                    $data['gender'] = $line['C'];
+                    $data['class'] = $line['D'];
+                    if ($model->create($data)) {
+                        if ($model->add()) {
+                            $result[] = "[成功]学号：$data[id]";
+                        } else {
+                            $errCount++;
+                            $result[] = "[失败]学号：$data[id]，错误信息：" . $model->getDbError();
+                        }
+                    } else {
+                        $errCount++;
+                        $result[] = "[失败]学号：$data[id]，错误信息：" . $model->getError();
+                    }
+                }
+                unlink($path);
+                if ($errCount) {
+                    $this->error(implode('<br>', $result), U('Student/manage'));
+                } else {
+                    $this->success(implode('<br>', $result), U('Student/manage'));
+                }
+            } catch (Exception $e) {
+                unlink($path);
+                $this->error($e->getMessage(), U('Student/manage'));
+            }
+        }
+    }
 }
